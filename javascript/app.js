9125c7d872655ee4807f088536c979ce54e1e432
LukMon <monikaa.lukowska@gmail.com>
2016-07-11 00:12:09 +0200

    Added compiled phpmailer to the index html and event.preventDefault to the app.js file.

--- javascript/app.js
+++ javascript/app.js
@@ -1,3 +1,22 @@
 document.addEventListener("DOMContentLoaded", function(){
-  console.log("Działa");
+
+  function stopPageReload(){
+    $( document ).ready(function() {
+       $("#contactForm").submit(function( event ){
+           event.preventDefault();
+           $.ajax({
+               url: 'send.php',
+               data: $("#contactForm").serialize(),
+               type: 'POST',
+               success: function( data ){
+                   $(".btn").html('<div style="background-color: green; padding: 5px; width: 100%; border-radius: 5px; color: white; "> We have received your request. Expect a feedback shortly.</div><br />');
+
+               }
+           });
+       });
+    });
+  }
+
+  stopPageReload();
+
 });
