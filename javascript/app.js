c5ee42f05f1867f035d48dca69b44b9af2ba584e
LukMon <monikaa.lukowska@gmail.com>
2016-07-11 14:10:33 +0200

    Added some comments to the app js file.

--- javascript/app.js
+++ javascript/app.js
@@ -1,19 +1,20 @@
 document.addEventListener("DOMContentLoaded", function(){
 
   function stopPageReload(){
-    $( document ).ready(function() {
-       $("#contactForm").submit(function( event ){
-           event.preventDefault();
-           $.ajax({
-               url: 'send.php',
-               data: $("#contactForm").serialize(),
-               type: 'POST',
-               success: function( data ){
-                   $(".btn").html('<div style="background-color: green; padding: 5px; width: 100%; border-radius: 5px; color: white; "> We have received your request. Expect a feedback shortly.</div><br />');
 
-               }
-           });
-       });
+    $("#contactForm").submit(function(event){
+      //stop page reload
+      event.preventDefault();
+
+      //send contact form data to the php send file
+      $.ajax({
+        url: 'send.php',
+        data: $("#contactForm").serialize(),
+        type: 'POST',
+        success: function(data){
+          $(".btn").html('<div style="background-color: green; padding: 5px; width: 100%; border-radius: 5px; color: white; "> We have received your request. Expect a feedback shortly.</div><br />');
+        }
+      });
     });
   }
 
