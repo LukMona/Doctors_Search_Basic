155975b150616900ae8280d72bc58eb6c8f97887
LukMon <monikaa.lukowska@gmail.com>
2017-01-08 20:06:29 +0100

    update

--- javascript/app.js
+++ javascript/app.js
@@ -1,3 +1,23 @@
 document.addEventListener("DOMContentLoaded", function(){
-  console.log("Działa");
+
+  function stopPageReload(){
+
+    $("#contactForm").submit(function(event){
+      //stop page reload
+      event.preventDefault();
+
+      //send contact form data to the php send file
+      $.ajax({
+        url: 'send.php',
+        data: $("#contactForm").serialize(),
+        type: 'POST',
+        success: function(data){
+          $(".btn").html('<div style="background-color: green; padding: 5px; width: 100%; border-radius: 5px; color: white; "> We have received your request. Expect a feedback shortly.</div><br />');
+        }
+      });
+    });
+  }
+
+  stopPageReload();
+
 });
